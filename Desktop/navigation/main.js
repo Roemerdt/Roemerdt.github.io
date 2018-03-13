7f77445df935eaa80927400c1c78697b8ec945b9
Roemer Blom <roemer@MacBook-Pro-van-Roemer.local>
2018-03-13 18:58:05 +0100

    initial commit

---
+++ Desktop/navigation/main.js
@@ -0,0 +1,17 @@
+var navigation = document.getElementById('navigation');
+var burger_toggle = document.getElementById('burger_toggle');
+var dropdown_menu = document.getElementById('dropdown');
+dropdown_menu.style.display = 'none';
+
+burger_toggle.addEventListener('mouseup', function() {
+	navigation.style.background = dropdown_menu.style.display === 'none' ? '#fff' : '';
+	dropdown.style.background = dropdown_menu.style.display === 'none' ? '#fff' : '';
+	dropdown_menu.style.display = dropdown_menu.style.display === 'none' ? 'block' : 'none';
+});
+
+window.addEventListener('resize', function(e) {
+	if(e.target.innerWidth > 980) {
+		navigation.style.background = '';
+		dropdown_menu.style.display = 'none';
+	}
+});
